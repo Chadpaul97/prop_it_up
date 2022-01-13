5f135350073e453a6c046ccbd9f231d8bf8c3103
Chadpaul97 <chadpaul97@gmail.com>
2022-01-13 12:52:18 -0600

    Birthday Increases with click

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import PersonComponent from './components/MyNewComponent';
 function App() {
   return (
     <div className="App">
-      <PersonComponent firstName={"John"} lastName={"Doe"} age ={"32"} hairColor = {"Brown"}/>
-      <PersonComponent firstName={"Brian"} lastName={"Smith"} age ={"28"} hairColor = {"Yellow"}/>
-      <PersonComponent firstName={"Hailey"} lastName={"Ryan"} age ={"34"} hairColor = {"Green"}/>
-      <PersonComponent firstName={"Jessica"} lastName={"Win"} age ={"19"} hairColor = {"Blue"}/>
+      <PersonComponent firstName={"John"} lastName={"Doe"} age ={32} hairColor = {"Brown"}/>
+      <PersonComponent firstName={"Brian"} lastName={"Smith"} age ={28} hairColor = {"Yellow"}/>
+      <PersonComponent firstName={"Hailey"} lastName={"Ryan"} age ={34} hairColor = {"Green"}/>
+      <PersonComponent firstName={"Jessica"} lastName={"Win"} age ={91} hairColor = {"Blue"}/>
     </div>
   );
 }
--- src/components/MyNewComponent.js
+++ src/components/MyNewComponent.js
@@ -1,13 +1,27 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-class PersonComponent extends Component{
-    render(){
-        const {firstName, lastName, age, hairColor} = this.props
-        return(
+class PersonComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            counter: this.props.age
+        }
+    }
+
+    addAge=() => {
+        this.setState({counter: this.state.counter + 1});
+    }
+
+    render() {
+        const { firstName, lastName, age, hairColor } = this.props
+
+
+        return (
             <div>
                 <h1>{firstName}, {lastName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.counter}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={this.addAge}>Birthday Button for {firstName} {lastName}</button>
             </div>
         )
     }
